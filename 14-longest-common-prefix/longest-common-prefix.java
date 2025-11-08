fd9c171872fcc4c0d79d26200ad729ee98f30a20
VasilevYuV <VasilevYuV23@st.ithub.ru>
2025-11-08 20:00:01 +0300

    Time: 0 ms (100.00%) | Memory: 43.3 MB (5.28%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -3,9 +3,9 @@ class Solution {
         if (strs == null || strs.length == 0) return "";
         String prefix = strs[0];
 
-        for (int j = 0; j < strs.length; j++){
+        for (int j = 1; j < strs.length; j++){
             while (!strs[j].startsWith(prefix)){
-                prefix = prefix.substring(0, prefix.length() -1);
+                prefix = prefix.substring(0, prefix.length() - 1);
                 if (prefix.equals("")) return "";
             }
         }
