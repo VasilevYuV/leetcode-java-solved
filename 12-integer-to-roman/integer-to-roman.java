b7ffc2250f81f9e1bca9185f2540670ce50c2447
VasilevYuV <VasilevYuV23@st.ithub.ru>
2025-11-08 17:50:41 +0300

    Time: 12 ms (7.79%) | Memory: 47.2 MB (5.24%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String intToRoman(int num) {
+        Map<Integer, String> vocabulary = new LinkedHashMap<>();
+        vocabulary.put(1000, "M");
+        vocabulary.put(900, "CM");
+        vocabulary.put(500, "D");
+        vocabulary.put(400, "CD");
+        vocabulary.put(100, "C");
+        vocabulary.put(90, "XC");
+        vocabulary.put(50, "L");
+        vocabulary.put(40, "XL");
+        vocabulary.put(10, "X");
+        vocabulary.put(9, "IX");
+        vocabulary.put(5, "V");
+        vocabulary.put(4, "IV");
+        vocabulary.put(1, "I");
+
+        StringBuilder romanValue = new StringBuilder();
+
+        for (int i = 0; i < vocabulary.size(); i++){
+            if (num == 0){
+                break;
+            }
+            while (num >= (int)vocabulary.keySet().toArray()[i]){
+                romanValue.append(vocabulary.values().toArray()[i]);
+                num -= (int)vocabulary.keySet().toArray()[i];
+            }
+        }
+
+        return romanValue.toString();
+    }
+}
