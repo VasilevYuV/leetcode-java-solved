842cf1de6d00f255df06398cf403f4fd8b884b67
VasilevYuV <VasilevYuV23@st.ithub.ru>
2025-11-26 18:08:55 +0300

    Time: 0 ms (100.00%) | Memory: 44.2 MB (19.07%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -16,12 +16,11 @@ class Solution {
             if (list1.val > list2.val){
                 newList.next = list2;
                 list2 = list2.next;
-                newList = newList.next;
             } else {
                 newList.next = list1;
                 list1 = list1.next;
-                newList = newList.next;
             }
+            newList = newList.next;
         }
         if (list2 != null){
             newList.next = list2;
