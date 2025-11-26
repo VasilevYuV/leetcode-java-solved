448aa7ae538d0f6be85831fd77b2fa9f47b1563a
VasilevYuV <VasilevYuV23@st.ithub.ru>
2025-11-26 18:41:59 +0300

    Time: 5 ms (36.31%) | Memory: 46.6 MB (30.79%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -9,29 +9,25 @@
  * }
  */
 class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null) return l2;
-        if (l2 == null) return l1;
-
-        if (l1.val < l2.val) {
-            l1.next = mergeTwoLists(l1.next, l2);
-            return l1;
-        } else {
-            l2.next = mergeTwoLists(l1, l2.next);
-            return l2;
-        }
-    }
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists.length == 0) return null;
-        return divideAndConquer(lists, 0, lists.length - 1);
-    }
+        PriorityQueue<Integer> minHeapPQ = new PriorityQueue<>();
+        for(ListNode head : lists){
+            ListNode temp = head;
 
-    private ListNode divideAndConquer(ListNode[] lists, int left, int right) {
-        if (left == right) return lists[left];
+            while(temp != null){
+                minHeapPQ.add(temp.val);
+                temp = temp.next;
+            }
+        }
 
-        int mid = left + (right - left) / 2;
-        ListNode l1 = divideAndConquer(lists, left, mid);
-        ListNode l2 = divideAndConquer(lists, mid + 1, right);
-        return mergeTwoLists(l1, l2);
+        ListNode newList = new ListNode(0);
+        ListNode head = newList;
+
+        while(minHeapPQ.size() > 0){
+            int num = minHeapPQ.poll();
+            head.next = new ListNode(num);
+            head = head.next;
+        }
+        return newList.next;
     }
 }
